ad799c8f21b5c79e7ab7a801f39342ac641f9daa
antony waweru <anto.waweru2030@gmail.com>
2023-05-23 11:54:00 +0300

    cookies and sessions debunked

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,17 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0 # Set initial value if not present
+    session[:page_views] += 1 # Increment page views
+
+    if session[:page_views] <= 3
+      # Render article data
+      article = Article.find(params[:id])
+      render json: article
+    else
+      # Render error message 
+      render json: { error: 'Maximum pageview limit reached' }, status: :unauthorized
+    end
   end
 
   private
